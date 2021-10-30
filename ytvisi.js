d77a4abd43b0704f7cab9ffd853ac33a64d670ae
dareducky <76420385+dareducky@users.noreply.github.com>
2021-10-30 17:10:17 -0300

    Update ytvisi.js

--- ytvisi.js
+++ ytvisi.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Video Background Play Fix
+// @namespace    https://greasyfork.org/en/users/50-couchy
+// @version      1.1
+// @description  Prevents YouTube and Vimeo from pausing videos when minimizing or switching tabs. Cross-browser port of https://addons.mozilla.org/en-US/firefox/addon/video-background-play-fix/
+// @author       Couchy
+// @match        *://*.youtube.com/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
 document.addEventListener('visibilitychange', function (e) { e.stopImmediatePropagation(); }, true);
  
 Object.defineProperty(document, 'visibilityState', { get: function () { return "visible"; } });
